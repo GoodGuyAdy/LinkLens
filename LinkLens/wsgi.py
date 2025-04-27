03f3376d186da9e3969b407f544cf27047ecd9fe
AdityaDubeySolytics <aditya.dubey@solytics-partners.com>
2025-04-27 14:50:51 +0530

    Initialized Django Project

---
+++ LinkLens/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for LinkLens project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LinkLens.settings")
+
+application = get_wsgi_application()
